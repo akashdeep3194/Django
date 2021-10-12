2558507f844018547fbf4f76436bec0b39f34beb
Akash Deep <akashdeep3194@gmail.com>
2021-10-12 18:10:46 +0530

    commit

--- todo_app/todo_app/views.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
-from rest_framework import status
-
-def index(request):
-    message = "This is a test message"
-    return Response(data=message,status=status.HTTP_200_OK)
