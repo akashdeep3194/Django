4b9921362db82a417a3c668fc1575a0d8cc194ee
Akash Deep <akashdeep3194@gmail.com>
2021-10-12 18:08:43 +0530

    commit

--- todo_test/todo_test/urls.py
+++ todo_test/todo_test/urls.py
@@ -1,4 +1,4 @@
-"""todo_app URL Configuration
+"""todo_test URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -14,9 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path , include
-from todo_app.todo_app.views import index
-from todo_app.views import index
+from django.urls import path
+from todo_test.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
