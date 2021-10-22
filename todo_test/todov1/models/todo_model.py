e2e9a6b56c526f35412f90f20f44e5ef60b35a84
Akash Deep <akashdeep3194@gmail.com>
2021-10-22 13:55:16 +0530

    commit

--- todo_test/todov1/models/todo_model.py
+++ todo_test/todov1/models/todo_model.py
@@ -1,10 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.deletion import CASCADE
+from django.db.models.deletion import CASCADE, DO_NOTHING
 
 # Create your models here.
 class Todo(models.Model):
     todo = models.TextField(verbose_name='Todos')
     addt = models.TextField(verbose_name="Additional data", null=True)
     created_date = models.DateField(auto_now=True)
-    user = models.ForeignKey(User, verbose_name="Username", on_delete=CASCADE)
+    user = models.ForeignKey(User, verbose_name="Username", on_delete=DO_NOTHING) # donothing
