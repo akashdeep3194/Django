182f42de2bd5254ab668e3193f89daddd06a279c
Akash Deep <akashdeep3194@gmail.com>
2021-11-09 23:31:13 +0530

    commit

--- todov1/urls.py
+++ todov1/urls.py
@@ -5,6 +5,7 @@ from todov1.views.register_view import RegisterView
 
 urlpatterns = [
     path('signup/',RegisterView.as_view(),name="RegisterView"),
+    path('register/',RegisterView.as_view(),name="RegisterView"),
     path('todos/', TodoView.as_view(), name="TodoView"),
     path('todos/<int:pk>', TodoView.as_view(), name="getTodoById"),
     path('todos/all', allTodos.as_view(), name="alltheTodos"),
